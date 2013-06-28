8069acee376443c9aefca67482cfee021c82efbb
Eldon Ahrold <eahrold@loyno.edu>
2013-06-28 07:20:34 -0500

    / ‘Security-Keychain/Security-Keychain.xcodeproj/project.xcworkspace/xcuserdata/eldon.xcuserdatad/UserInterfaceState.xcuserstate’ / ‘Security-Keychain/Security-Keychain/AppDelegate.m’

--- Security-Keychain/Security-Keychain/AppDelegate.m
+++ Security-Keychain/Security-Keychain/AppDelegate.m
@@ -38,7 +38,7 @@
 
 - (IBAction)getPressed:(id)sender{
     NSLog(@"Get Pressed");
-    NSString *result = [user stringValue];
+    NSString *result = self.user.stringValue;
     NSLog(@"Get Pressed %@",result);
 }
 @end
