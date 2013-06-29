76d7acff5c772161a4cd36be37f6bd1bfa1c7c9a
Eldon Ahrold <eahrold@loyno.edu>
2013-06-28 20:42:12 -0500

    / ‘Security-Keychain/Security-Keychain.xcodeproj/project.xcworkspace/xcuserdata/eldon.xcuserdatad/UserInterfaceState.xcuserstate’ / ‘Security-Keychain/Security-Keychain/AppDelegate.m’

--- Security-Keychain/Security-Keychain/AppDelegate.m
+++ Security-Keychain/Security-Keychain/AppDelegate.m
@@ -33,7 +33,7 @@
     userName = (CFStringRef)CFPreferencesCopyAppValue(userNameKey,
                                                        kCFPreferencesCurrentApplication);
     // When finished with value, you must release it
-    if(! userName){
+    if(userName ! ){
     CFRelease(userName);
     }
 }
