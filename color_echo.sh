f0672a5fc5287f90b2d46fb9369901284449f753
Eldon Ahrold <eahrold@loyno.edu>
2013-10-04 11:37:14 -0500

    / ‘color_echo.sh’

--- color_echo.sh
+++ color_echo.sh
@@ -23,10 +23,10 @@ cecho(){
 	fi
 
 	local RESET=$(printf "\\e[0m")	
-	echo "${COLOR}${MESSAGE}${RESET}${3}"	
+	echo "${COLOR}${MESSAGE}${RESET} ${3}"	
 }
 
-cecho alert "hello my alert" "what's new"
+cecho alert "hello my alert:" "what's new"
 cecho warn "hello my warn"
 cecho attention "hello my attention"
 cecho notice "hello my notice"
