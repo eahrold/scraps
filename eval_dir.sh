c44d310c01a4141e75dcd4fbb0c4858c9c6d4e66
Eldon Ahrold <eahrold@loyno.edu>
2013-10-10 09:34:37 -0500

    / ‘eval_dir.sh’

--- eval_dir.sh
+++ eval_dir.sh
@@ -6,19 +6,19 @@ eval_dir(){
 # pass the name of the variable you want to eval
 # so you would pass MYVAR rather than $MYVAR
 eval local __myvar=${!1}
-	if [ $? == 0 ]; then
-		local __tilda=$(echo ${__myvar} | cut -c1)
-		if [ ${__tilda} == '~' ]; then
-			eval local __home="~"
-			__myvar="${__home}${__myvar:1}"
-		fi
-			
-		local __len=${#__myvar}-1
-		if [ "${__myvar:__len}" != "/" ]; then
-		  __myvar=$__myvar"/"
-		fi
-		eval $1="'$__myvar'"
-	else
-		return 1
+if [ $? == 0 ]; then
+	local __tilda=$(echo ${__myvar} | cut -c1)
+	if [ ${__tilda} == '~' ]; then
+		eval local __home="~"
+		__myvar="${__home}${__myvar:1}"
 	fi
+		
+	local __len=${#__myvar}-1
+	if [ "${__myvar:__len}" != "/" ]; then
+	  __myvar=$__myvar"/"
+	fi
+	eval $1="'$__myvar'"
+else
+	return 1
+fi
 }
