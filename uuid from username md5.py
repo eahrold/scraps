76648c49b8918ffbf0e24c2f9bb4ef596860af81
Eldon Ahrold <eahrold@loyno.edu>
2014-08-26 20:39:48 -0500

    / ‘uuid from username md5.py’

--- uuid from username md5.py
+++ uuid from username md5.py
@@ -2,8 +2,16 @@
 
 import hashlib
 import re
-string="test4"
+string="kaserna"
 
 m = hashlib.md5()
 m.update(string)
 print re.sub(r'[a-zAND0]',r'',m.hexdigest())[-6:]
+
+
+'''
+10280929
+Serna
+Kali
+kaserna
+'''
